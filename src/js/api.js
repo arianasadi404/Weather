0e09b8a627200e6f888547b0eb205c340cbd4360
arianasadi404 <arianasadi404@gmail.com>
2026-05-12 18:01:25 +0330

    Replace XMLHttpRequest with Axios

--- src/js/api.js
+++ src/js/api.js
@@ -1,34 +1,36 @@
+import axios from "axios";
 import { getWeatherDescription } from "./ui.js";
 import { temperature, humidity, wind, statusText } from "./elements.js";
 
-// FETCH WEATHER DATA**********
-const xhr = new XMLHttpRequest();
-xhr.open(
-  "GET",
-  "https://api.open-meteo.com/v1/forecast?latitude=35.6875&longitude=51.375&current=temperature_2m,relative_humidity_2m,windspeed_10m,weather_code",
-  true,
-);
+const BASE_URL = "https://api.open-meteo.com/v1/forecast";
 
-// Handle API response**********
-xhr.addEventListener("readystatechange", () => {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    let weatherData = JSON.parse(xhr.responseText);
+async function fetchWeather() {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        latitude: 35.6875,
+        longitude: 51.375,
+        current:
+          "temperature_2m,relative_humidity_2m,windspeed_10m,weather_code",
+      },
+    });
 
-    // Update DOM with weather data**********
+    const weatherData = response.data;
+
     temperature.innerHTML = weatherData.current.temperature_2m + "°C";
     humidity.innerHTML = weatherData.current.relative_humidity_2m + "%";
     wind.innerHTML = weatherData.current.windspeed_10m + "km/h";
-    getWeatherDescription(weatherData.current.weather_code);
-  } else if (xhr.readyState === 4 && xhr.status !== 200) {
-    statusText.innerHTML = "خطا: " + xhr.status;
-  }
-});
-xhr.send();
 
-// Handle network errors**********
-xhr.onerror = function () {
-  statusText.innerHTML = "خطا در دریافت اطلاعات";
-  temperature.innerHTML = "---";
-  wind.innerHTML = "---";
-  humidity.innerHTML = "---";
-};
+    getWeatherDescription(weatherData.current.weather_code);
+  } catch (error) {
+    statusText.innerHTML = "خطا در دریافت اطلاعات";
+
+    temperature.innerHTML = "---";
+    wind.innerHTML = "---";
+    humidity.innerHTML = "---";
+
+    console.error(error);
+  }
+}
+
+fetchWeather();
